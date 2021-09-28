f5768f7fdb43a928d10bf81ddf23b7128a0433a8
Ahmed M.Osman <64021350+akiid777@users.noreply.github.com>
2021-09-28 22:54:15 +0200

    Create Arrays.cs

---
+++ Arrays.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace challange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = new int[] { 10 , 11 , 12 , 13 , 14 , 15};
+            Console.WriteLine(student[3]);
+
+
+
+            var studentGradus = new int[10];
+            studentGradus[0] = 80;
+            studentGradus[1] = 90;
+            studentGradus[2] = 100;
+            studentGradus[9] = 900;
+
+            Console.WriteLine(studentGradus[9]);
+        }
+        }
+        }
